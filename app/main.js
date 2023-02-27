a9e7d81e2712f954e9b99cf09045cdf7294b602d
Andrey Sinyavskiy <sinerda@gmail.com>
2023-02-27 12:54:13 +0200

    New function for other company data

--- app/main.js
+++ app/main.js
@@ -14,7 +14,11 @@ $(function () {
   let
     storageTitle = $('#storage-title span'),
     transferTitle = $('#transfer-title span');
-  let scalewayPrice;
+  let
+    backblazePrice,
+    bunnyPrice,
+    scalewayPrice,
+    vultrPrice;
 
   $().ready(ChangeDate);
   $(`#storage, 
@@ -32,6 +36,39 @@ $(function () {
     }
   }
 
+  function BackblazeValues(Storage, Transfer) {
+    backblazePrice = (Storage * 0.005) + (Transfer * 0.01);
+
+    if (backblazePrice < 7) {
+      backblazePrice = 7;
+    }
+
+    backblazePrice = Number(backblazePrice.toFixed(2));
+    backblazeText.text(backblazePrice);
+    FlipMobile(backblaze, backblazePrice);
+  }
+
+  function BunnyValues(Storage, Transfer) {
+    let bunnyCheck = $('input[name=bunny]:checked').val();
+
+    switch (bunnyCheck) {
+      case 'hdd':
+        bunnyPrice = (Storage * 0.01) + (Transfer * 0.01);
+        break;
+      case 'ssd':
+        bunnyPrice = (Storage * 0.02) + (Transfer * 0.01);
+        break;
+    }
+
+    if (bunnyPrice > 10) {
+      bunnyPrice = 10;
+    }
+
+    bunnyPrice = Number(bunnyPrice.toFixed(2));
+    bunnyText.text(bunnyPrice);
+    FlipMobile(bunny, bunnyPrice);
+  }
+
   function ScalewayValues(Storage, Transfer) {
     if ((Storage <= 75) && (Transfer <= 75)) {
       scalewayPrice = 0;
@@ -42,28 +79,40 @@ $(function () {
       switch (scalewayCheck) {
         case 'multi':
           if (Storage <= 75) {
-            scalewayPrice = ((Transfer - 75) * 0.02).toFixed(2);
+            scalewayPrice = (Transfer - 75) * 0.02;
           }
           else if (Transfer <= 75) {
-            scalewayPrice = ((Storage - 75) * 0.06).toFixed(2);
+            scalewayPrice = (Storage - 75) * 0.06;
           }
-          else scalewayPrice = (((Storage - 75) * 0.06) + ((Transfer - 75) * 0.02)).toFixed(2);
+          else scalewayPrice = ((Storage - 75) * 0.06) + ((Transfer - 75) * 0.02);
           break;
         case 'single':
           if (Storage <= 75) {
-            scalewayPrice = ((Transfer - 75) * 0.02).toFixed(2);
+            scalewayPrice = (Transfer - 75) * 0.02;
           }
           else if (Transfer <= 75) {
-            scalewayPrice = ((Storage - 75) * 0.03).toFixed(2);
+            scalewayPrice = (Storage - 75) * 0.03;
           }
-          else scalewayPrice = (((Storage - 75) * 0.03) + ((Transfer - 75) * 0.02)).toFixed(2);
+          else scalewayPrice = ((Storage - 75) * 0.03) + ((Transfer - 75) * 0.02);
           break;
       }
     }
-    
-    scalewayPrice = Number(scalewayPrice);
+
+    scalewayPrice = Number(scalewayPrice.toFixed(2));
     scalewayText.text(scalewayPrice);
     FlipMobile(scaleway, scalewayPrice);
+  }
+
+  function VultrValues(Storage, Transfer) {
+    vultrPrice = (Storage * 0.01) + (Transfer * 0.01);
+
+    if (vultrPrice < 5) {
+      vultrPrice = 5;
+    }
+    
+    vultrPrice = Number(vultrPrice.toFixed(2));
+    vultrText.text(vultrPrice);
+    FlipMobile(vultr, vultrPrice);
   }
 
   function ChangeDate() {
@@ -74,48 +123,10 @@ $(function () {
     storageTitle.text(Storage);
     transferTitle.text(Transfer);
 
-    // backblaze
-    let backblazePrice = ((Storage * 0.005) + (Transfer * 0.01)).toFixed(2);
-    backblazePrice = Number(backblazePrice);
-    if (backblazePrice < 7) {
-      backblazePrice = 7;
-    }
-    backblazeText.text(backblazePrice);
-    FlipMobile(backblaze, backblazePrice);
-
-    // bunny
-    let bunnyCheck = $('input[name=bunny]:checked').val();
-    let bunnyPrice;
-
-    switch (bunnyCheck) {
-      case 'hdd':
-        bunnyPrice = ((Storage * 0.01) + (Transfer * 0.01)).toFixed(2);
-        break;
-      case 'ssd':
-        bunnyPrice = ((Storage * 0.02) + (Transfer * 0.01)).toFixed(2);
-        break;
-    }
-
-    bunnyPrice = Number(bunnyPrice);
-    if (bunnyPrice > 10) {
-      bunnyPrice = 10;
-    }
-    bunnyText.text(bunnyPrice);
-    FlipMobile(bunny, bunnyPrice);
-
-    // scaleway
+    BackblazeValues(Storage, Transfer);
+    BunnyValues(Storage, Transfer);
     ScalewayValues(Storage, Transfer);
-
-    // vultr
-    let vultrPrice = ((Storage * 0.01) + (Transfer * 0.01)).toFixed(2);
-
-    vultrPrice = Number(vultrPrice);
-    if (vultrPrice < 5) {
-      vultrPrice = 5;
-    }
-    vultrText.text(vultrPrice);
-    FlipMobile(vultr, vultrPrice);
-
+    VultrValues(Storage, Transfer);
 
     let priceArray = [backblazePrice, bunnyPrice, scalewayPrice, vultrPrice];
     let graphArray = [backblaze, bunny, scaleway, vultr];
