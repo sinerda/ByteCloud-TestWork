338a5fcb831c7d507bfdddd5eaa37c3c502a61c0
Andrey Sinyavskiy <sinerda@gmail.com>
2023-03-04 15:02:33 +0200

    New architecture with classes

--- app/main.js
+++ app/main.js
@@ -1,43 +1,130 @@
 "use strict";
 
 $(function () {
-  // 
-  // Объекты
-  let backblaze = {
-    price: 0,
-    text: $('#backblaze-text span'),
-    chart: $('#backblaze'),
 
-    calculate(Storage, Transfer) {
-      this.price = (Storage * 0.005) + (Transfer * 0.01);
+  class Company {
+
+    constructor(nameCompany) {
+      this.price = 0;
+      this.text = $(`#${nameCompany}-text span`);
+      this.chart = $(`#${nameCompany}`);
+    }
+
+    rendering(storage, transfer) {
+      this.calculate(storage, transfer);
+      this.finalActions(this.chart, this.price, this.text);
+    };
+
+    finalActions(company, price, text) {
+      price = Number(price.toFixed(2));
+      text.text(price);
+      this.chartDrawing(company, price);
+    }
+
+    chartDrawing(company, price) {
+      const _630px = window.matchMedia("(max-width: 630px)");
+
+      if (!_630px.matches) {
+        company.css('height', price * 4);
+      } else {
+        company.css('width', price * 5);
+      }
+    }
+
+  }
+
+  class Backblaze extends Company {
+
+    calculate(storage, transfer) {
+      this.price = (storage * 0.005) + (transfer * 0.01);
       if (this.price < 7) {
         this.price = 7;
       }
       return this.price;
-    },
-    rendering(Storage, Transfer) {
-      this.calculate(Storage, Transfer);
-      finalActions(this.chart, this.price, this.text);
     }
+
   }
 
-  // - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - - 
-  const
-    // backblaze = $('#backblaze'),
-    bunny = $('#bunny'),
-    scaleway = $('#scaleway'),
-    vultr = $('#vultr'),
-    // backblazeText = $('#backblaze-text span'),
-    bunnyText = $('#bunny-text span'),
-    scalewayText = $('#scaleway-text span'),
-    vultrText = $('#vultr-text span');
+  class Bunny extends Company {
+
+    calculate(storage, transfer) {
+      let bunnyCheck = $('input[name=bunny]:checked').val();
+
+      switch (bunnyCheck) {
+        case 'hdd':
+          this.price = (storage * 0.01) + (transfer * 0.01);
+          break;
+        case 'ssd':
+          this.price = (storage * 0.02) + (transfer * 0.01);
+          break;
+      }
+      if (this.price > 10) {
+        this.price = 10;
+      }
+      return this.price;
+    }
+
+  }
+
+  class Scaleway extends Company {
+
+    calculate(storage, transfer) {
+      if ((storage <= 75) && (transfer <= 75)) {
+        this.price = 0;
+      }
+      else {
+        let scalewayCheck = $('input[name=scaleway]:checked').val();
+
+        switch (scalewayCheck) {
+          case 'multi':
+            if (storage <= 75) {
+              this.price = (transfer - 75) * 0.02;
+            } else if (transfer <= 75) {
+              this.price = (storage - 75) * 0.06;
+            } else {
+              this.price = ((storage - 75) * 0.06) + ((transfer - 75) * 0.02);
+              break;
+            }
+          case 'single':
+            if (storage <= 75) {
+              this.price = (transfer - 75) * 0.02;
+            } else if (transfer <= 75) {
+              this.price = (storage - 75) * 0.03;
+            } else {
+              this.price = ((storage - 75) * 0.03) + ((transfer - 75) * 0.02);
+              break;
+            }
+        }
+      }
+      return this.price;
+    }
+
+  }
+
+  class Vultr extends Company {
+
+    calculate(storage, transfer) {
+      this.price = (storage * 0.01) + (transfer * 0.01);
+      if (this.price < 5) {
+        this.price = 5;
+      }
+      return this.price;
+    }
+
+  }
+
+  // 
+  // Объекты
+  let
+    backblaze = new Backblaze('backblaze'),
+    bunny = new Bunny('bunny'),
+    scaleway = new Scaleway('scaleway'),
+    vultr = new Vultr('vultr');
+  // 
+
   let
     storageTitle = $('#storage-title span'),
-    transferTitle = $('#transfer-title span'),
-    // backblazePrice,
-    bunnyPrice,
-    scalewayPrice,
-    vultrPrice;
+    transferTitle = $('#transfer-title span');
 
   $().ready(changeDate);
   $(`#storage, 
@@ -49,105 +136,27 @@ $(function () {
   // Основная функция
   function changeDate() {
     let
-      Storage = $('#storage').val(),
-      Transfer = $('#transfer').val();
+      storage = $('#storage').val(),
+      transfer = $('#transfer').val();
 
-    storageTitle.text(Storage);
-    transferTitle.text(Transfer);
+    storageTitle.text(storage);
+    transferTitle.text(transfer);
 
-    backblaze.rendering(Storage, Transfer);
-    bunnyValues(Storage, Transfer);
-    scalewayValues(Storage, Transfer);
-    vultrValues(Storage, Transfer);
+    backblaze.rendering(storage, transfer);
+    bunny.rendering(storage, transfer);
+    scaleway.rendering(storage, transfer);
+    vultr.rendering(storage, transfer);
 
     coloringMinimumValue();
-  }
-
-  // 
-  // Алгоритмы подсчёта значений
-  // - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - - 
-  function bunnyValues(Storage, Transfer) {
-    let bunnyCheck = $('input[name=bunny]:checked').val();
-
-    switch (bunnyCheck) {
-      case 'hdd':
-        bunnyPrice = (Storage * 0.01) + (Transfer * 0.01);
-        break;
-      case 'ssd':
-        bunnyPrice = (Storage * 0.02) + (Transfer * 0.01);
-        break;
-    }
-    if (bunnyPrice > 10) {
-      bunnyPrice = 10;
-    }
-
-    finalActions(bunny, bunnyPrice, bunnyText);
-  }
-
-  function scalewayValues(Storage, Transfer) {
-    if ((Storage <= 75) && (Transfer <= 75)) {
-      scalewayPrice = 0;
-    }
-    else {
-      let scalewayCheck = $('input[name=scaleway]:checked').val();
-
-      switch (scalewayCheck) {
-        case 'multi':
-          if (Storage <= 75) {
-            scalewayPrice = (Transfer - 75) * 0.02;
-          } else if (Transfer <= 75) {
-            scalewayPrice = (Storage - 75) * 0.06;
-          } else {
-            scalewayPrice = ((Storage - 75) * 0.06) + ((Transfer - 75) * 0.02);
-            break;
-          }
-        case 'single':
-          if (Storage <= 75) {
-            scalewayPrice = (Transfer - 75) * 0.02;
-          } else if (Transfer <= 75) {
-            scalewayPrice = (Storage - 75) * 0.03;
-          } else {
-            scalewayPrice = ((Storage - 75) * 0.03) + ((Transfer - 75) * 0.02);
-            break;
-          }
-      }
-    }
-
-    finalActions(scaleway, scalewayPrice, scalewayText);
-  }
-
-  function vultrValues(Storage, Transfer) {
-    vultrPrice = (Storage * 0.01) + (Transfer * 0.01);
-    if (vultrPrice < 5) {
-      vultrPrice = 5;
-    }
-
-    finalActions(vultr, vultrPrice, vultrText);
   }
   // - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - - 
 
   // 
   // Вспомогательные функции
   // - -  - -  - -  - -  - -  - -  - -  - -  - -  - -  - - 
-  function finalActions(Company, Price, Text) {
-    Price = Number(Price.toFixed(2));
-    Text.text(Price);
-    chartDrawing(Company, Price);
-  }
-
-  function chartDrawing(Company, Price) {
-    const _630px = window.matchMedia("(max-width: 630px)");
-
-    if (!_630px.matches) {
-      Company.css('height', Price * 4);
-    } else {
-      Company.css('width', Price * 5);
-    }
-  }
-
   function coloringMinimumValue() {
-    let priceArray = [backblaze.price, bunnyPrice, scalewayPrice, vultrPrice];
-    let graphArray = [backblaze.chart, bunny, scaleway, vultr];
+    let priceArray = [backblaze.price, bunny.price, scaleway.price, vultr.price];
+    let graphArray = [backblaze.chart, bunny.chart, scaleway.chart, vultr.chart];
 
     // убираем предыдущие классы
     for (let i = 0; i < graphArray.length; i++) {
